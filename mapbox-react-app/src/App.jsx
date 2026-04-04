261977102e5ac490157cbbfa9bb30cacf6f965c8
Jason Andersen <andersen.jason7@gmail.com>
2026-04-03 19:38:49 -0600

    adding LDS icons and symbols

--- mapbox-react-app/src/App.jsx
+++ mapbox-react-app/src/App.jsx
@@ -6,7 +6,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css'
 
 const accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
-const center = [-71.05953, 42.36290];
+const center = [-104.05953, 40.36290];
 
 function App() {
 
@@ -51,8 +51,35 @@ function App() {
         }
       });
 
+//add the temple layer to the map
+//first add the image to the map
+
+  mapRef.current.loadImage('/temple-icon.png', (error, image) => {
+    if (error) throw error;
+    mapRef.current.addImage('temple-icon', image);
+  });
 
 
+  // Add another source/layer
+  mapRef.current.addSource('temples', {
+    type: 'geojson',
+    data: '/ChurchofJesusChristTemples.geojson' // or your own URL/path
+  });
+
+  mapRef.current.addLayer({
+    id: 'temples',
+    type: 'symbol',
+    source: 'temples',
+    layout: {
+      'icon-image': 'temple-icon', // Use the loaded image as the icon
+      'icon-size': 0.5, // Adjust size as needed
+      'icon-allow-overlap': true // Allow icons to overlap if necessary
+    },
+    paint: {
+      'icon-opacity': 1,  // Optional: adjust opacity
+   
+    }
+  });
       // Change cursor on hover
       mapRef.current.on('mouseenter', 'earthquakes', (e) => {
         mapRef.current.getCanvas().style.cursor = 'pointer';
@@ -90,14 +117,53 @@ function App() {
       });
 
             // Remove popup on mouse leave
-      mapRef.current.on('mouseleave', 'earthquakes', () => {
+      mapRef.current.on('mouseleave', 'temples', () => {
+        mapRef.current.getCanvas().style.cursor = '';
+        popupRef.current.remove();
+      });
+
+       // Change cursor on hover
+      mapRef.current.on('mouseenter', 'temples', (e) => {
+        mapRef.current.getCanvas().style.cursor = 'pointer';
+
+        // Get coordinates from the feature
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const [longitude, latitude] = coordinates;
+
+        const properties = e.features[0].properties;
+        const templeId = properties.Name || 'N/A';
+        const templePhone = properties.Phone || 'N/A';
+
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        // Set popup content and location
+        popupRef.current
+          .setLngLat(coordinates)
+          .setHTML(`
+            <div style="padding: 8px;">
+              <strong>Location</strong><br/>
+              <strong>Name:</strong> ${templeId}<br/>
+              <strong>Phone:</strong> ${templePhone}<br/>
+              Latitude: ${latitude.toFixed(4)}<br/>
+              Longitude: ${longitude.toFixed(4)}
+            </div>
+          `)
+          .addTo(mapRef.current);
+      });
+
+            // Remove popup on mouse leave
+      mapRef.current.on('mouseleave', 'temples', () => {
         mapRef.current.getCanvas().style.cursor = '';
         popupRef.current.remove();
       });
 
 
-
-        
     });
 
     return () => {
